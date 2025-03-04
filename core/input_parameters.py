57449bf44ea8535c448bab5ce6e62a05d87de479
dwatremez <donatien.watremez@gmail.com>
2025-03-04 08:06:41 +0100

    input validation is now embedded into  computation logic files

--- core/input_parameters.py
+++ core/input_parameters.py
@@ -1,5 +1,5 @@
 from core.constants import IdiomuscularReactionType, RigorType, LividityType, LividityDisappearanceType, \
-    LividityMobilityType, BodyCondition, EnvironmentType, SupportingBase, TEMPERATURE_LIMITS, TemperatureLimitsType, BODY_MASS_LIMIT
+    LividityMobilityType, BodyCondition, EnvironmentType, SupportingBase
 
 
 class InputParameters:
@@ -61,40 +61,3 @@ class InputParameters:
         self.lividity_disappearance = lividity_disappearance
         self.lividity_mobility = lividity_mobility
         self.user_corrective_factor = user_corrective_factor
-
-    def validate(self) -> bool:
-        """
-        Validation of members of input parameters
-        
-        Returns
-        -------
-        True 
-            if inputs are valid
-        
-        Raises
-        -------
-        ValueError
-            if some inputs are not valid        
-        """
-
-        error_message = []
-
-        # Verification of temperature limits
-        ambient_limits = TEMPERATURE_LIMITS.get(TemperatureLimitsType.AMBIENT)
-        if not (ambient_limits[0] <= self.ambient_temperature <= ambient_limits[1]):
-            error_message.append(f"The ambient temperature ({self.ambient_temperature}°C) is not valid and must be between {ambient_limits[0]}°C and {ambient_limits[1]}°C.")
-
-        rectal_limits = TEMPERATURE_LIMITS.get(TemperatureLimitsType.RECTAL)
-        if not (rectal_limits[0] <= self.rectal_temperature <= rectal_limits[1]):
-            error_message.append(f"The rectal temperature ({self.rectal_temperature}°C) is not valid and must be between {rectal_limits[0]}°C and {rectal_limits[1]}°C.")
-
-        # Verification of body mass limits
-        if not (BODY_MASS_LIMIT[0] <= self.body_mass <= BODY_MASS_LIMIT[1]):
-            error_message.append(f"The body mass ({self.body_mass}kg) is not valid and must be between {BODY_MASS_LIMIT[0]}kg and {BODY_MASS_LIMIT[1]}kg.")
-
-        # Raise error if some values are not valid
-        if len(error_message) > 0:
-            raise ValueError('\n'.join(error_message))
-
-        # Returns true if everything is valid
-        return True
